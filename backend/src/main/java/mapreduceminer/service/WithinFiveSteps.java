795c4b41656e228caf0aa507a24dba1985381efd
sensati0n <christian_sturm.voh@gmx.de>
2018-11-21 10:14:36 +0100

    Test and Custom Constraint

---
+++ backend/src/main/java/mapreduceminer/service/WithinFiveSteps.java
@@ -0,0 +1,73 @@
+package mapreduceminer.service;
+
+import de.ubt.ai4.mapreduceminer.Database;
+import de.ubt.ai4.mapreduceminer.constraint.DoubleEventConstraint;
+import de.ubt.ai4.mapreduceminer.constraint.Eventbased;
+import de.ubt.ai4.mapreduceminer.constraint.FutureBased;
+import de.ubt.ai4.mapreduceminer.model.Event;
+import de.ubt.ai4.mapreduceminer.result.ResultElement;
+import de.ubt.ai4.mapreduceminer.util.AuxiliaryDatabase;
+import de.ubt.ai4.mapreduceminer.util.ConstraintType;
+import mapreduceminer.service.CustomAuxiliaryDatabase;
+
+public class WithinFiveSteps extends DoubleEventConstraint implements Eventbased, FutureBased {
+
+    public WithinFiveSteps(Event eventA, Event eventB, ConstraintType type) {
+        super(eventA, eventB, type);
+    }
+
+    public WithinFiveSteps() {
+        
+    }
+
+
+    @Override
+	public boolean logic(AuxiliaryDatabase ad) {
+
+        CustomAuxiliaryDatabase cad = (CustomAuxiliaryDatabase) ad;
+
+        if(cad.currentJ < cad.currentI+1)
+            return false;
+
+        if(cad.currentJ > cad.currentI+5)
+            return false;
+            
+        Event filteredEventB = super.getEventB();
+        switch (super.getType()) {
+            case ACTIVATION:
+                filteredEventB  = filteredEventB.filter(super.getEventIdentifier());
+                break;
+            case TARGET:
+                filteredEventB = filteredEventB.filter(super.getEventIdentifier(), super.getAdditionalAttribute());
+                break;
+            case CORRELATION:
+        }
+
+        if (!cad.tasksWithinFiveSteps.contains(filteredEventB)) {
+            cad.tasksWithinFiveSteps.add(filteredEventB);
+            {System.out.println("true");
+            return true;}
+        } else
+            return false;
+        }
+
+
+    @Override
+    public ResultElement getResult(Database db, double sigma, int logSize) {
+        double eta = db.getEta().get(getEventA());
+        double support = sigma / eta;
+
+        int currentEpsilon = db.getEpsilon().get(getEventA());
+        double confidence = support * (currentEpsilon / (double) logSize);
+
+        //System.out.println("Support(" + constraint.getName() + currentEntry.getKey() + ") = \t\t" + support);
+        //System.out.println("Confidence(" + currentEntry.getKey() + ") = \t" + confidence);
+        return new ResultElement(this.getClass().toString(), getEventA(), getEventB(), support, confidence, this.getType());
+    }
+
+   
+
+    
+
+
+}
---
+++ backend/src/test/java/mapreduceminer/IntegrationTest.java
@@ -0,0 +1,63 @@
+package mapreduceminer;
+
+import javax.servlet.ServletContext;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import junit.framework.Assert;
+import mapreduceminer.ApiController;
+import mapreduceminer.Application;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@SpringBootTest
+@WebMvcTest(Application.class)
+public class IntegrationTest {
+    
+    @Autowired MockMvc mockMvc;
+
+  /*  @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(this.apiController).build();
+    }
+*/
+
+
+private String xmlMock =  "<?xml version='1.0' encoding='UTF-8'?><eventLog><trace>        <event>            <string key='task' value='a'/>            <string key='resource' value='x'/>        </event>        <event>            <string key='task' value='c'/>            <string key='resource' value='z'/>        </event>        <event>            <string key='task' value='b'/>            <string key='resource' value='y'/>        </event>        <event>            <string key='task' value='b'/>            <string key='resource' value='x'/>        </event>        <event>            <string key='task' value='d'/>            <string key='resource' value='z'/>        </event>        <event>            <string key='task' value='b'/>            <string key='resource' value='y'/>        </event>        <event>            <string key='task' value='a'/>            <string key='resource' value='x'/>        </event>    </trace>    <trace>        <event>            <string key='task' value='a'/>            <string key='resource' value='x'/>        </event>        <event>            <string key='task' value='b'/>            <string key='resource' value='x'/>        </event>        <event>            <string key='task' value='b'/>            <string key='resource' value='y'/>        </event>        <event>            <string key='task' value='c'/>            <string key='resource' value='x'/>        </event>    </trace>	<trace>        <event>            <string key='task' value='a'/>            <string key='resource' value='y'/>        </event>        <event>            <string key='task' value='c'/>            <string key='resource' value='x'/>        </event>        <event>            <string key='task' value='d'/>            <string key='resource' value='y'/>        </event>    </trace>	<number>100</number></eventLog>";
+
+@Test
+public void testLog() throws Exception {
+/*
+    mockMvc.perform(get("/ctrler"))
+    .andExpect(status().isOk());
+
+
+    String url = "miningJob";
+
+
+    mockMvc.perform(post(url).contentType(MediaType.APPLICATION_XML_VALUE)
+        .content(xmlMock))
+        .andExpect(status().isOk());
+    
+   /* this.mockMvc.perform(post("/miningewwewJob"))
+
+    .content(xmlMock)
+    .contentType(MediaType.APPLICATION_XML_VALUE));*/
+}
+    
+
+}
